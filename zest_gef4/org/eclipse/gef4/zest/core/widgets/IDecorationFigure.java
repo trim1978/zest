89d07b99f21c47efa2470700edc1906583e35b68
Konstantin <slider78@rambler.ru>
2025-03-31 01:27:48 +0500

    1

---
+++ zest_gef4/org/eclipse/gef4/zest/core/widgets/IDecorationFigure.java
@@ -0,0 +1,9 @@
+package org.eclipse.gef4.zest.core.widgets;
+
+/**
+ * Decoration figure interface
+ *
+ */
+public interface IDecorationFigure 
+{
+}
---
+++ zest_gef4/org/eclipse/gef4/zest/core/widgets/IDecorationLayer.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package org.eclipse.gef4.zest.core.widgets;
+
+/**
+ * Interface for additional decoration layers
+ */
+public interface IDecorationLayer
+{
+}
